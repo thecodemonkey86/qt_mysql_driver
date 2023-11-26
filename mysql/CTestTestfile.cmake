aa027b05222238193860b605ac0010f6b678b3ee
Bernhard Stöcker <bernhard.stoecker@aktivweb.de>
2023-11-26 13:03:25 +0100

    Update Qt 6.6.1

--- mysql/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Qt/Qt5.12/6.6.0/Src/qtbase/src/plugins/sqldrivers/mysql
-# Build directory: C:/Qt/Qt5.12/6.6.0/Src/qtbase/src/plugins/sqldrivers/mysql
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
