661413ea5b3d38e2c473c6c8da7178c3de9e88a3
Bernhard Stöcker <bernhard.stoecker@aktivweb.de>
2024-09-27 10:15:07 +0200

    update Qt 6.7.3

--- CTestTestfile.cmake
+++ CTestTestfile.cmake
@@ -1,10 +1,9 @@
 # CMake generated Testfile for 
-# Source directory: C:/Qt/Qt5.12/6.6.0/Src/qtbase/src/plugins/sqldrivers
-# Build directory: C:/Qt/Qt5.12/6.6.0/Src/qtbase/src/plugins/sqldrivers
+# Source directory: C:/Qt/6.7.3/Src/qtbase/src/plugins/sqldrivers
+# Build directory: C:/Qt/6.7.3/Src/qtbase/src/plugins/sqldrivers
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-subdirs("psql")
 subdirs("mysql")
 subdirs("odbc")
 subdirs("sqlite")
---
+++ mysql/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Qt/6.7.3/Src/qtbase/src/plugins/sqldrivers/mysql
+# Build directory: C:/Qt/6.7.3/Src/qtbase/src/plugins/sqldrivers/mysql
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
